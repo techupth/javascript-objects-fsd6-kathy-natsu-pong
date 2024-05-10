49a7398a0bfef20b0999dff1aafc5b011a3dcc73
KathyKZ <kathykate.sooksutum@gmail.com>
2024-05-10 12:47:29 +0700

    feat: add object hw

--- ex-3.js
+++ ex-3.js
@@ -9,21 +9,10 @@ const employee = {
     city: "Cityville",
     states: "Stateville",
     country: "Countryland",
-    postalCode: "12345",
+    postalCode: 12345,
   },
 };
 
-console.log("Employee Name: " + employees.name);
+console.log("Employee Name: " + employee.name);
 console.log("Employee Age: " + employee.age);
-console.log(
-  "Employee Address: " +
-    employee.address.street +
-    ", " +
-    employee.address.city +
-    ", " +
-    employee.address.state +
-    ", " +
-    employee.address.country +
-    " " +
-    employee.adress.postalCode
-);
+console.log("Employee Address: " + employee.address.street + ", " + employee.address.city + ", " + employee.address.states + ", " + employee.address.country + " " + employee.address.postalCode);
